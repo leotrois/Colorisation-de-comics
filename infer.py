da6255439298b3eddae366f3371b11a88c755fd5
unknown <you@example.com>
2024-11-21 16:57:09 +0100

    Now the model can infer

--- infer.py
+++ infer.py
@@ -3,33 +3,33 @@ from torchvision.io import read_image
 from torchvision import transforms
 from torch import permute
 from torchvision.utils import save_image
-
+import torch
 
 PRETRAINED = True
 IMG_SIZE = (512, 512)
 
-
+import os 
+os.chdir(r"C:\Users\Léo\Desktop\Mini_projet_pour_git\Colorisation-de-comics")
 if PRETRAINED:
-    PATH = "Pix2Pix_weights_10_epochs.pt"
+    PATH = "weights_pre_trained.pt"
 else:
     PATH = "Weights_trained.pt"
     
 device = torch.device('cuda' if torch.cuda.is_available() else torch.device('cpu'))
     
 if __name__ == "__main__":
+    test = torch.load
     model = Pix2Pix()
-    model.load_state_dict(PATH, weights_only=True)
-    model.eval()
+    model.load_state_dict(torch.load(PATH, weights_only=True, map_location=torch.device(device) ))
     
     model.to(device)
     
-    image = read_image('./infer_image.jpg')
+    image = read_image('./image_test.jpg')
     image = transforms.Resize(IMG_SIZE)(image)
     image = (image - 127.5) / 127.5
     image = image.unsqueeze(0).to(device)
     image = model.forward(image)
-    image = squeeze(image)
-    image = permute(image, (1,2,0))
-
+    image = torch.squeeze(image)
     save_image(image,"./result.jpg")
+    
     
--- train.py
+++ train.py
@@ -1,16 +1,17 @@
 from models import Pix2Pix
 from torch.utils.data import DataLoader, random_split
 from classes import CustomDataset
+import torch
 
 
-
-BATCH_SIZE = 32
+BATCH_SIZE = 1
 IMG_SIZE = (512,512)
 EPOCHS = 10
 device = torch.device('cuda' if torch.cuda.is_available() else torch.device('cpu'))
 
-
-data = CustomDataset("dataset.csv", IMG_SIZE)
+import os
+os.chdir(r"C:\Users\Léo\Desktop\Mini_projet_Automatants")
+data = CustomDataset(r"C:\Users\Léo\Desktop\Mini_projet_pour_git\Colorisation-de-comics\dataset.csv", IMG_SIZE, device = device)
 
 train_size = int(0.9 * len(data))
 test_size = len(data) - train_size
@@ -22,6 +23,7 @@ train_dataset, test_dataset = random_split(data, [train_size, test_size])
 x_train = DataLoader(train_dataset, batch_size = BATCH_SIZE, shuffle = True)
 x_test = DataLoader(train_dataset, 1, shuffle = True)
 
-if __name__ = "__main__":
+if __name__ == "__main__":
+    model = Pix2Pix().to(device)
     model.train(EPOCHS, x_train)
     torch.save(model.state_dict(), "Weights_trained.pt")
